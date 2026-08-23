32a5ae946e89d30b4d2a1be01796f96343351f5d
andrearonchi25-lgtm <andrearonchi25@gmail.com>
2026-08-23 16:30:12 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE='raise-your-hammer-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
+  const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
+}).catch(()=>caches.match('./index.html')))));
