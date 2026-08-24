7730d5155f7c4dfe64e22b0bd255c37711bb2279
andrearonchi25-lgtm <andrearonchi25@gmail.com>
2026-08-24 11:48:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-const CACHE='raise-your-hammer-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{
-  const copy=x.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return x;
-}).catch(()=>caches.match('./index.html')))));
+const CACHE='ryh-v47-final-2';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return; e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>r).catch(()=>caches.match(e.request)))})
